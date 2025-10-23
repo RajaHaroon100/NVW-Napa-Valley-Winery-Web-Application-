e21f265fbfb69635ce05bb339dbe878062c7d922
Muhammad Haroon <87198210+RajaHaroon100@users.noreply.github.com>
2025-10-24 00:25:19 +0500

    Update route.js

--- .next/server/app/api/admin/bookings/route.js
+++ .next/server/app/api/admin/bookings/route.js
@@ -1,1 +1,9 @@
+const CHUNK_PUBLIC_PATH = "server/app/api/admin/bookings/route.js";
+const runtime = require("../../../../chunks/[turbopack]_runtime.js");
+runtime.loadChunk("server/chunks/node_modules_0ebee8._.js");
+runtime.loadChunk("server/chunks/[root of the server]__e28abe._.js");
+runtime.loadChunk("server/chunks/src_models_winery_model_ts_324065._.js");
+runtime.loadChunk("server/chunks/_f3332b._.js");
+runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/admin/bookings/route/actions.js [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH);
+module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/admin/bookings/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
 
